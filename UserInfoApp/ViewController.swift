245b6a1f4c4389adedb1efa654cb94c59c81315b
l.zapatagt <l.zapatagt@gmail.com>
2019-10-17 19:16:18 -0300

    Initial Commit

---
+++ UserInfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInfoApp
+//
+//  Created by Luis Zapata on 17-10-19.
+//  Copyright © 2019 Luis Zapata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
